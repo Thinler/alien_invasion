6b65114c9314fe9312f913e305bfbaeb2ebc34bc
thinler <yyh.email@sjtu.edu.cn>
2023-06-29 19:37:12 +0800

    increase difficulties

--- alien_invasion.py
+++ alien_invasion.py
@@ -98,6 +98,7 @@ class AlienInvasion:
     def _check_play_button(self,mouse_pos):
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.settings.initialize_dynamic_settings()
             # 重置游戏统计信息
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -155,6 +156,7 @@ class AlienInvasion:
             #delete all bullets and create a group of aliens
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """check out if there is any alien at the edge pf screen and
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ class Settings:
     """存储所有设置的类"""
 
     def __init__(self):
+        """初始化游戏的静态设置"""
         # screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -18,6 +19,23 @@ class Settings:
         self.bullets_allowed = 3
 
         # 外星人设置
-        self.alien_speed = 0.5
         self.fleet_drop_speed = 5
+
+        # 加快游戏节奏的难度
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
         self.fleet_direction = 1 # 1 move right, -1 move left
+
+    def increase_speed(self):
+        self.ship_speed *=self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
